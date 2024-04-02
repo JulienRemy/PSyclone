f850b73cb7b288014875cd398e8636086820b56e
JulienRemy <lesjardinsdegamare@gmail.com>
2024-04-02 12:13:28 +0200

    Leftover debug printing.

--- src/psyclone/autodiff/transformations/reverse_mode/ad_reverse_routine_trans.py
+++ src/psyclone/autodiff/transformations/reverse_mode/ad_reverse_routine_trans.py
@@ -1391,7 +1391,7 @@ class ADReverseRoutineTrans(ADRoutineTrans):
         if use_sympy:
             for ref in self.recording.walk(ArrayReference):
                 if ref.name == tape.name:
-                    print(f"Simplifying {ref.name}")
+                    # print(f"Simplifying {ref.name}")
                     ref.replace_with(
                         tape.simplify_expression_with_sympy(
                             ref, self.recording_table
